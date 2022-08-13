b200ec4a6c9e403d08b2707bd8ea6c98959d76b2
Kudin <91375108+Alexander18101995@users.noreply.github.com>
2022-08-13 20:28:18 +0300

    Add files via upload

---
+++ re.py
@@ -0,0 +1,43 @@
+import re 
+import csv
+with open("phonebook_raw.csv") as f:
+  rows = csv.reader(f, delimiter=",")
+  contacts_list = list(rows)
+  contacts_list[1][1] = contacts_list[1][0][9:13]
+  contacts_list[1][2] = contacts_list[1][0][14:]
+  contacts_list[1][0] = contacts_list[1][0][:8]
+  contacts_list[4][5] = contacts_list[2][5]
+  del contacts_list[4][6]
+  del contacts_list[2]
+  contacts_list[2][2] = contacts_list[2][1][9:]
+  contacts_list[2][1] = contacts_list[2][1][:8]
+  contacts_list[4][1] = contacts_list[4][0][7:12]
+  contacts_list[4][2] = contacts_list[4][0][13:]
+  contacts_list[4][0] = contacts_list[4][0][:6]
+  contacts_list[5][1] = contacts_list[5][0][8:15]
+  contacts_list[5][2] = contacts_list[5][0][16:]
+  contacts_list[5][0] = contacts_list[5][0][:7]
+  contacts_list[7][1] = contacts_list[7][0][9:]
+  contacts_list[7][0] = contacts_list[7][0][:8]
+  contacts_list[7][2] = contacts_list[6][0][14:]
+  contacts_list[7][3] = contacts_list[6][3]
+  contacts_list[7][5] = contacts_list[6][5]
+  del contacts_list[6] 
+  pattern = r"(\+7)\s\((\d+)\)\s*(\d+)-(\d+)-(\d+)"
+  pattern_2 = r"(\+7)(\d{3})(\d{3})(\d{2})(\d{2})"
+  pattern_3 = r"(8)\((\d+)\)(\d+)-(\d+)-(\d+)"
+  pattern_4 = r"(8)\s(\d+)-(\d+)-(\d{2})(\d+)"
+  r_p = r"+7(\2)\3-\4-\5"
+  result = re.sub(pattern,r_p,contacts_list)
+  result_2 = re.sub(pattern_2,r_p,result)
+  result_3 = re.sub(pattern_3,r_p,result_2)
+  result_4 = re.sub(pattern_4,r_p,result_3)
+  pattern_5 = r"(\+7)\((\d+)\)(\d+)-(\d+)-(\d+)\s\((\w+.)\s(\w+)\)"
+  r_p_2 = r"+7(\2)\3-\4-\5 \6\7"
+  result_5 = re.sub(pattern_5,r_p_2,result_4)
+  pattern_6 = r"(\+7)\((\d+)\)(\d+)-(\d+)-(\d+)\s(\w+.)\s(\d+)"
+  r_p_2 = r"+7(\2)\3-\4-\5 \6\7"
+  result_6 = re.sub(pattern_6,r_p_2,result_5)
+  with open("phonebook.csv", "w") as f:
+   datawriter = csv.writer(f, delimiter=',')
+   datawriter.writerows(result_6)
