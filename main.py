d8d9fe7d964f63f92b41cfb9409ed242a5294dbb
Kudin <91375108+Alexander18101995@users.noreply.github.com>
2022-08-26 09:54:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,23 +23,27 @@ with open("phonebook_raw.csv") as f:
   contacts_list[7][3] = contacts_list[6][3]
   contacts_list[7][5] = contacts_list[6][5]
   del contacts_list[6]
-  with open("phonebook.csv", "w") as f:
-    for cl in contacts_list:
-      res = ','.join(cl)
-      pattern = r"(\+7)\s\((\d+)\)\s*(\d+)-(\d+)-(\d+)"
-      pattern_2 = r"(\+7)(\d{3})(\d{3})(\d{2})(\d{2})"
-      pattern_3 = r"(8)\((\d+)\)(\d+)-(\d+)-(\d+)"
-      pattern_4 = r"(8)\s(\d+)-(\d+)-(\d{2})(\d+)"
-      r_p = r"+7(\2)\3-\4-\5"
-      result = re.sub(pattern,r_p,res)
-      result_2 = re.sub(pattern_2,r_p,result)
-      result_3 = re.sub(pattern_3,r_p,result_2)
-      result_4 = re.sub(pattern_4,r_p,result_3)
-      pattern_5 = r"(\+7)\((\d+)\)(\d+)-(\d+)-(\d+)\s\((\w+.)\s(\w+)\)"
-      r_p_2 = r"+7(\2)\3-\4-\5 \6\7"
-      result_5 = re.sub(pattern_5,r_p_2,result_4)
-      pattern_6 = r"(\+7)\((\d+)\)(\d+)-(\d+)-(\d+)\s(\w+.)\s(\d+)"
-      r_p_2 = r"+7(\2)\3-\4-\5 \6\7"
-      result_6 = re.sub(pattern_6,r_p_2,result_5)
-      new_contacts_list = result_6
-      datawriter = f.write(new_contacts_list)
+with open("phonebook.csv", "w") as f:
+  for cl in contacts_list:
+    res = ','.join(cl)
+    pattern = r"(\+7)\s\((\d+)\)\s*(\d+)-(\d+)-(\d+)"
+    pattern_2 = r"(\+7)(\d{3})(\d{3})(\d{2})(\d{2})"
+    pattern_3 = r"(8)\((\d+)\)(\d+)-(\d+)-(\d+)"
+    pattern_4 = r"(8)\s(\d+)-(\d+)-(\d{2})(\d+)"
+    r_p = r"+7(\2)\3-\4-\5"
+    result = re.sub(pattern,r_p,res)
+    result_2 = re.sub(pattern_2,r_p,result)
+    result_3 = re.sub(pattern_3,r_p,result_2)
+    result_4 = re.sub(pattern_4,r_p,result_3)
+    pattern_5 = r"(\+7)\((\d+)\)(\d+)-(\d+)-(\d+)\s\((\w+.)\s(\w+)\)"
+    r_p_2 = r"+7(\2)\3-\4-\5 \6\7"
+    result_5 = re.sub(pattern_5,r_p_2,result_4)
+    pattern_6 = r"(\+7)\((\d+)\)(\d+)-(\d+)-(\d+)\s(\w+.)\s(\d+)"
+    r_p_2 = r"+7(\2)\3-\4-\5 \6\7"
+    result_6 = re.sub(pattern_6,r_p_2,result_5)
+    list_1 = []
+    new_contacts_list = []
+    list_1.append(result_6)
+    new_contacts_list.append(list_1)
+    data = csv.writer(f)
+    data.writerows(new_contacts_list)
